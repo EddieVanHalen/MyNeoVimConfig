1538f237c36463699e423f732489948522a8fa7a
EddieVanHalen <atillaismayil94@gmail.com>
2024-11-30 15:20:29 +0400

    Adding VimPlug

--- init.lua
+++ init.lua
@@ -1,26 +1,32 @@
 vim.cmd("let mapleader = ' '")
 
+vim.cmd [[
+  call plug#begin('~/.local/share/nvim/plugged')
+  Plug 'vim-airline/vim-airline'
+  call plug#end()
+]]
+
 require("preferences")
 require("installing-lazy")
 require("keymaps")
 require("icons").setup()
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	nested = true,
-	callback = function()
-		vim.cmd.colorscheme(vim.g.SCHEME)
-	end,
+  nested = true,
+  callback = function()
+    vim.cmd.colorscheme(vim.g.SCHEME)
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-	callback = function(params)
-		vim.g.SCHEME = params.match
-	end,
+  callback = function(params)
+    vim.g.SCHEME = params.match
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
-    callback = function()
-        vim.lsp.buf.format({ async = false })
-    end,
+  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
 })
